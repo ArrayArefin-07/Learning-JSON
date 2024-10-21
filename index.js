b4c0a145b0c9b90e8b6800386e564f69f99b37fc
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-10-21 20:26:38 +0600

    Learned JSON path Access, modify, delete

---
+++ index.js
@@ -0,0 +1,12 @@
+const data = require("./friendsData.json");
+// console.log(data);
+// console.log(data.friends[0]);
+
+// console.log(data.friends[0].name);
+
+// change json data
+// data.friends[0].name = "Oakil Sarker";
+// console.log(data.friends[0].name)
+
+// delete data.friends[0].name;
+// console.log(data);
